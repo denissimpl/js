2aea0cac6e53b25434dfd0d6d6fb229c525a38c3
denis <159763707+denissimpl@users.noreply.github.com>
2024-02-19 11:49:45 +0500

    searching-elements-dom

---
+++ searching-elements-dom/index.js
@@ -0,0 +1,7 @@
+let table = document.getElementById("age-table")
+let els = table.querySelectorAll("label")
+let td = table.rows[0].cells[0]
+let form = document.getElementsByName('search')[0]
+let firstinput = form.getElementsByTagName('input')[0]
+let inputs = form.querySelectorAll('input') 
+inputs[inputs.length-1]
