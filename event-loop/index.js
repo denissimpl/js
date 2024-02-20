a48e38cd70a566e1be4efb462204ce30bc0cd484
denis <159763707+denissimpl@users.noreply.github.com>
2024-02-20 14:30:47 +0500

    event-loop

---
+++ event-loop/index.js
@@ -0,0 +1,31 @@
+// setTimeout(function timeout() {
+//     console.log('Таймаут'); 4
+// }, 0);
+
+// let p = new Promise(function(resolve, reject) {
+//     console.log('Создание промиса'); 1
+//     resolve();
+// });
+
+// p.then(function(){
+//     console.log('Обработка промиса'); 3
+// }); 
+
+// console.log('Конец скрипта'); 2
+
+
+// console.log(1); 
+
+// setTimeout(() => console.log(2));
+
+// Promise.resolve().then(() => console.log(3));
+
+// Promise.resolve().then(() => setTimeout(() => console.log(4)));
+
+// Promise.resolve().then(() => console.log(5));
+
+// setTimeout(() => console.log(6));
+
+// console.log(7);
+
+// 1735264
