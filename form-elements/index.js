e5d3c919c0dbe18dd12426de38f6408a36065b13
denis <159763707+denissimpl@users.noreply.github.com>
2024-02-20 12:52:08 +0500

    form-elements

---
+++ form-elements/index.js
@@ -0,0 +1,7 @@
+{/* <select id="genres">
+  <option value="rock">Рок</option>
+  <option value="blues" selected>Блюз</option>
+</select> */}
+let classic = new Option("Классика", "classic", false, false)
+genres.append(classic)
+classic.selected = true
